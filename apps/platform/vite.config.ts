da7164d98ee614725ea691c85289a90a5244bd08
bone-house <bonehouse.dev@gmail.com>
2024-02-12 16:58:42 +0100

    Update vite.config.ts

--- apps/platform/vite.config.ts
+++ apps/platform/vite.config.ts
@@ -7,10 +7,7 @@ export default defineConfig(() => ({
   envPrefix: ENV_PREFIX,
   server: { port: 4001, host: false },
   assetsInclude: ['**/*.glb'],
-  define: {
-    'process.env.ANCHOR_BROWSER': true,
-    global: 'window',
-  },
+  define: { 'process.env.ANCHOR_BROWSER': true },
   resolve: { alias: { crypto: 'crypto-browserify' } },
   plugins: [
     react({ jsxRuntime: 'classic' }),

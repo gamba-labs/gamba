a720398884866a1a65671150c7e4ed8c9a1d67e4
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-05-21 12:09:02 -0400

    Update docs
    - Add gamba API docs
    - Update homepage
    - Clean packages
    - ect..

--- apps/website/components/projects.ts
+++ apps/website/components/projects.ts
@@ -2,41 +2,41 @@ export const projects = [
   {
     name: "Guacamole GG",
     link: "https://www.guacamole.gg/play",
-    thumbnail: "/thumbnails/guac.png"
+    thumbnail: "/thumbnails/guac.png",
   },
   {
     name: "Oh Yeah Bet",
     link: "https://www.ohyeah.bet/",
-    thumbnail: "/thumbnails/ohyeah.png"
+    thumbnail: "/thumbnails/ohyeah.png",
   },
   {
     name: "Fronk Casino",
     link: "https://casino.fronk.xyz/",
-    thumbnail: "/thumbnails/fronk.png"
+    thumbnail: "/thumbnails/fronk.png",
   },
   {
     name: "Sol Bets",
     link: "https://www.solbets.app/",
-    thumbnail: "/thumbnails/solbets.png"
+    thumbnail: "/thumbnails/solbets.png",
   },
   {
     name: "SKB Gaming",
     link: "https://skbgaming.solkillerbunnies.io/",
-    thumbnail: "/thumbnails/skbgaming.png"
+    thumbnail: "/thumbnails/skbgaming.png",
   },
   {
     name: "Felt Gaming Casino",
     link: "https://casino.feltgaming.io/",
-    thumbnail: "/thumbnails/feltgaming.png"
+    thumbnail: "/thumbnails/feltgaming.png",
   },
   {
     name: "NinjaTSS Casino",
     link: "https://www.ninjatsscasino.xyz/",
-    thumbnail: "/thumbnails/ninjaturtles.png"
+    thumbnail: "/thumbnails/ninjaturtles.png",
   },
   {
-    name: 'SolarMoon',
-    thumbnail: '/thumbnails/solarmoon.png',
-    link: 'https://flip.solarmoon.xyz/',
+    name: "SolarMoon",
+    thumbnail: "/thumbnails/solarmoon.png",
+    link: "https://flip.solarmoon.xyz/",
   },
-]
+];

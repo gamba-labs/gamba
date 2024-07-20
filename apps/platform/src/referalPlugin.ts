147627d61d6836ed148ce18a7c83db8f96c5a436
bone-house <bonehouse.dev@gmail.com>
2024-07-20 17:45:09 +0200

    added static sol fee fallback for referals

--- apps/platform/src/referalPlugin.ts
+++ apps/platform/src/referalPlugin.ts
@@ -1,6 +1,6 @@
 import * as SplToken from '@solana/spl-token'
 import '@solana/wallet-adapter-react-ui/styles.css'
-import { PublicKey, SystemProgram } from '@solana/web3.js'
+import { LAMPORTS_PER_SOL, PublicKey, SystemProgram } from '@solana/web3.js'
 import { GambaPlugin } from 'gamba-react-v2'
 import './styles.css'
 
@@ -8,18 +8,20 @@ import './styles.css'
  * The instructions returned from this plugin will be executed before gamba's "play" instruction
  */
 export const makeReferalPlugin = (
-  percent = 0.1,
+  tokenPercent = 0.1,
+  fixedSolFallback = 0.01,
 ): GambaPlugin => async (input, context) => {
   try {
     const _receiver = window.location.hash.slice(1)
     const receiver = new PublicKey(_receiver)
+
     // Send native SOL
     if (input.token.equals(SplToken.NATIVE_MINT)) {
       return [
         SystemProgram.transfer({
           fromPubkey: input.wallet,
           toPubkey: receiver,
-          lamports: input.wager * percent,
+          lamports: input.wager * tokenPercent,
         }),
       ]
     }
@@ -27,38 +29,58 @@ export const makeReferalPlugin = (
     const fromAta = SplToken.getAssociatedTokenAddressSync(input.token, input.wallet)
     const toAta = SplToken.getAssociatedTokenAddressSync(input.token, receiver)
 
-    const createAtaInstruction = async () => {
+    const recipientHasAta = await (async () => {
       try {
         await SplToken.getAccount(context.provider.anchorProvider.connection, toAta, 'confirmed')
         // Recipient account exists, return empty
-        return []
+        return true
       } catch (error) {
         if (error instanceof SplToken.TokenAccountNotFoundError || error instanceof SplToken.TokenInvalidAccountOwnerError) {
           // Recipient account doesnt exist, add create instruction
-          return [
-            SplToken.createAssociatedTokenAccountInstruction(
-              input.wallet,
-              toAta,
-              receiver,
-              input.token,
-            ),
-          ]
+          return false
         } else {
           throw error
         }
       }
+    })()
+
+    if (recipientHasAta) {
+      // Send SPL Token
+      return [
+        SplToken.createTransferInstruction(
+          fromAta,
+          toAta,
+          input.wallet,
+          BigInt(input.wager) / BigInt(1 / tokenPercent),
+        ),
+      ]
     }
 
-    // Send SPL token
+    // Default to sending a fixed amount of SOL if the recipient doesn't have an ATA
     return [
-      ...(await createAtaInstruction()),
-      SplToken.createTransferInstruction(
-        fromAta,
-        toAta,
-        input.wallet,
-        BigInt(input.wager) / BigInt(1 / percent),
-      ),
+      SystemProgram.transfer({
+        fromPubkey: input.wallet,
+        toPubkey: receiver,
+        lamports: fixedSolFallback * LAMPORTS_PER_SOL,
+      }),
     ]
+    // Alternatively: We can open an account for the recipient,
+    // however, this allows for an "exploit" where the recipient can close their ATA after each play,
+    // allowing them to collect SOL and making the player have to pay more
+    // return [
+    //   SplToken.createAssociatedTokenAccountInstruction(
+    //     input.wallet,
+    //     toAta,
+    //     receiver,
+    //     input.token,
+    //   ),
+    //   SplToken.createTransferInstruction(
+    //     fromAta,
+    //     toAta,
+    //     input.wallet,
+    //     BigInt(input.wager) / BigInt(1 / tokenPercent),
+    //   )
+    // ]
   } catch (err) {
     console.error(err)
     return []
